6f964c92a458b73fb519d28bf08bc7814e99aea7
Fumiya Amano <fumiya.amano@gree.co.jp>
2017-01-02 08:45:52 +0900

    add houseIcon animation

--- Assets/Scripts/Scene/MapScene/UI/HouseIconButton.cs
+++ Assets/Scripts/Scene/MapScene/UI/HouseIconButton.cs
@@ -28,4 +28,9 @@ public class HouseIconButton : CommonButton
 		onClickedHouseCallback(index);
 		AudioManager.Instance.PlaySE2("s-9_se");
 	}
+
+	public void Update()
+	{
+		iTween.ScaleTo(this.gameObject, iTween.Hash("scale", new Vector3(1.3f, 1.3f, 1), "time", 0.5f, "easetype", "easeInSine", "loopType", "pingPong"));
+	}
 }
