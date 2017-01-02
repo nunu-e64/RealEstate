ffe52456756983e4b295780f67ba8cb26024c656
Fumiya Amano <fumiya.amano@gree.co.jp>
2017-01-02 10:08:10 +0900

    update TItle start button

---
+++ Assets/StartButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+	public void Update()
+	{
+		iTween.ScaleTo(this.gameObject, iTween.Hash("scale", new Vector3(1.3f, 1.3f, 1), "time", 0.5f, "easetype", "easeInSine", "loopType", "pingPong"));
+	}
+}
